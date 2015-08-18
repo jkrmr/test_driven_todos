eddf5f54fb19669a201d2709fa405228a23ac3d1
Jake Romer <jkrmr@users.noreply.github.com>
2015-08-18 14:23:06 -0400

    Create user model [green]

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe User, type: :model do
+  it { should validate_presence_of(:email) }
+  it { should validate_uniqueness_of(:email) }
+
+  it 'validates the format of :email' do
+    user = build(:user)
+
+    user.email = 'email at example dot com'
+
+    expect(user).to_not be_valid
+  end
+end
