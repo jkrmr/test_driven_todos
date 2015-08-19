b429f521bf63e8968fc20866923b4da205a950e1
Jake Romer <jkrmr@users.noreply.github.com>
2015-08-18 22:41:15 -0400

    Extract CompletionsController#todo [refactor]

--- app/controllers/completions_controller.rb
+++ app/controllers/completions_controller.rb
@@ -1,6 +1,13 @@
 class CompletionsController < ApplicationController
   def create
-    current_user.todos.find_by(id: params[:todo_id]).touch(:completed_at)
+    todo.touch(:completed_at)
+
     redirect_to todos_url
+  end
+
+  private
+
+  def todo
+    current_user.todos.find_by(id: params[:todo_id])
   end
 end
